9e6067edb01851caf7e457a6b7c1248b937e3f74
Chufeng Li <chufengl@asu.edu>
2019-05-16 13:18:42 -0700

    for NSLS demo

--- NSLS_FMX_utils_mpis.py
+++ NSLS_FMX_utils_mpis.py
@@ -1,16 +1,17 @@
 #!python -u
 
 """
-Started on 3-8-2019 at NSLS 
+Started on 3-8-2019 at NSLS
 Authors: Chufeng Li, Nadia Zatsepin
 E-mail: chufengl@asu.edu
 Usage:
- 
-	NSLS_FMX_utils.py <Eiger_file_name> <thld> <min_pix> <mask_file> <min_peak>
+
+	NSLS_FMX_utils.py <Eiger_file_name> <thld> <min_pix> <max_pix> <mask_file> <min_peak> <Region>
 	thld: pixel value threshold
 	min_pix: minimal number of pixels for a peak
 	min_peak: minimal number of peaks for a hit
-        
+	Region: 'ALL', 'C', 'Q'
+
 """
 
 
@@ -22,7 +23,8 @@ import scipy
 import glob
 import h5py
 import mpi4py.MPI as MPI
-from pytictoc import TicToc 
+from pytictoc import TicToc
+#import fabio
 
 def Eiger_img_read(Eiger_file_name,frame_no):
 	'''
@@ -30,17 +32,17 @@ def Eiger_img_read(Eiger_file_name,frame_no):
 	frame number.
 	'''
 	f=h5py.File(Eiger_file_name,'r')
-	
+
 	img_arry=np.array(f['entry/data/data'][frame_no,:,:],dtype='int32')
 	print('The shape of the image is :',img_arry.shape)
 	f.close()
 	return img_arry
 
 def single_peak_finder(img_arry,Eiger_file_name,frame_no,thld,min_pix,mask_file='None',interact=False):
-	
-	
+
+
 	bimg=(img_arry>thld)
-	
+
 	if mask_file is not ' None':
 		mask_file=os.path.abspath(mask_file)
 		m=h5py.File(mask_file,'r')
@@ -50,7 +52,7 @@ def single_peak_finder(img_arry,Eiger_file_name,frame_no,thld,min_pix,mask_file=
 		mask=np.ones_like(img_arry).astype(bool)
 	else:
 		sys.exit('the mask file option is inproper.')
-	
+
 	bimg=bimg*mask
 	all_labels=measure.label(bimg)
 	props=measure.regionprops(all_labels,img_arry)
@@ -68,11 +70,11 @@ def single_peak_finder(img_arry,Eiger_file_name,frame_no,thld,min_pix,mask_file=
 	area_filtered_sorted=area_filtered[area_sort_ind]
 	weighted_centroid_filtered=np.zeros((len(label_filtered_sorted),2))
 	for index,value in enumerate(label_filtered_sorted):
-        
+
         	weighted_centroid_filtered[index,:]=np.array(props[value-1].weighted_centroid)
 #	print('In image: %s \n %5d peaks are found' %(img_file_name, len(label_filtered_sorted)))
 	beam_center=np.array([1492.98,2163.41])
-	
+
 	if interact:
 		plt.figure(figsize=(15,15))
 		plt.imshow(img_arry*(mask.astype(np.int16)),cmap='jet')
@@ -84,85 +86,114 @@ def single_peak_finder(img_arry,Eiger_file_name,frame_no,thld,min_pix,mask_file=
 		title_Str=Eiger_file_name+'\nEvent: %d '%(frame_no)
 		plt.title(title_Str)
 		plt.show()
-	return label_filtered_sorted,weighted_centroid_filtered,props 
+	return label_filtered_sorted,weighted_centroid_filtered,props
 
 
 
-def file_hit_finder_subset(Eiger_file_name,rank,size,skip,thld,min_pix,min_peak,mask_file='None',Region='ALL'):
-    if Region=='ALL':
-        x_min=0
-        y_min=0
-        x_max=4371
-        y_max=4150
-    elif Region=='Q':
-        x_min=2185
-        y_min=2075
-        x_max=4370
-        y_max=4150
-    elif Region=='C':
-        x_min=1092
-        y_min=1037
-        x_max=3278
-        y_max=3112
-    else:
-        sys.exit('Check the Region option: ALL,Q,C')
-    Eiger_file_name=os.path.abspath(Eiger_file_name)
-    db=h5py.File(Eiger_file_name,'r')
-    db_1=db['/entry/data/data']
-    job_total_load=db_1.shape[0]
-    job_per_load=np.ceil(job_total_load/size).astype(np.int16)
-    frame_list=np.arange(rank*job_per_load,np.minimum((rank+1)*job_per_load,job_total_load),skip)
-    img_block=np.array(db_1[frame_list,x_min:x_max,y_min:y_max])
-    db.close()
-    print('--------------')
-    print(Eiger_file_name)
-    print('the data block shape is: ',img_block.shape)
-    if mask_file!='None':
-        mask_file=os.path.abspath(mask_file)
-        m=h5py.File(mask_file,'r')
-        mask=m['/data/data'][x_min:x_max,y_min:y_max].astype(bool)
-        m.close()
-    elif mask_file=='None':
-        mask=np.ones_like(img_block[0,:,:]).astype(bool)
-    else:
-        sys.exit('the mask file option is inproper.')
+def file_hit_finder_subset(Eiger_file_name,rank,size,skip,thld,min_pix,max_pix,min_peak,mask_file='None',Region='ALL'):
+	if Region=='ALL':
+		x_min=0
+		y_min=0
+		x_max=4371
+		y_max=4150
+	elif Region=='Q':
+		x_min=2185
+		y_min=2075
+		x_max=4370
+		y_max=4150
+	elif Region=='C':
+		x_min=1092
+		y_min=1037
+		x_max=3278
+		y_max=3112
+	else:
+		sys.exit('Check the Region option: ALL,Q,C')
+	Eiger_file_name=os.path.abspath(Eiger_file_name)
+	db=h5py.File(Eiger_file_name,'r') #h5py read
+	db_1=db['/entry/data/data']       #h5py read
+	#db=fabio.open(Eiger_file_name)     #fabio read
+ 	#db_1=db.dataset[0]                 #fabio read
+	job_total_load=db_1.shape[0]
+	job_per_load=np.ceil(job_total_load/size).astype(np.int16)
+	frame_list=np.arange(rank*job_per_load,np.minimum((rank+1)*job_per_load,job_total_load),skip)
+	img_block=np.array(db_1[frame_list,x_min:x_max,y_min:y_max])  #h5py read
+	db.close()   #h5py read
 
-    HIT_counter=0
-    HIT_event_no_list=[]
-    for l in range(len(frame_list)):
-        event_no=frame_list[l]
-        img_arry=img_block[l,:,:]
-        bimg=(img_arry>thld)
-        bimg=bimg*mask
-        all_labels=measure.label(bimg)
-        props=measure.regionprops(all_labels,img_arry)
-        area=np.array([r.area for r in props]).reshape(-1,)
-        max_intensity=np.array([r.max_intensity for r in props]).reshape(-1,)
-        label=np.array([r.label for r in props]).reshape(-1,)
-        centroid=np.array([np.array(r.centroid).reshape(1,2) for r in props]).reshape((-1,2))
-        weighted_centroid=np.array([r.weighted_centroid for r in props]).reshape(-1,)
-        label_filtered=label[area>min_pix]
-        area_filtered=area[area>min_pix]
-        area_sort_ind=np.argsort(area_filtered)[::-1]
-        label_filtered_sorted=label_filtered[area_sort_ind]
-        area_filtered_sorted=area_filtered[area_sort_ind]
-        weighted_centroid_filtered=np.zeros((len(label_filtered_sorted),2))
-        for index,value in enumerate(label_filtered_sorted):
-            weighted_centroid_filtered[index,:]=np.array(props[value-1].weighted_centroid)
-        peak_no=len(area_filtered_sorted)
-        if peak_no>=min_peak:
-            HIT_counter+=1
-            HIT_event_no_list.append(event_no)
-            print('HIT!!!!  %d  Event: %d peaks found'%(event_no,peak_no))
-        else:
-            print('BLANK!   %d  Event: %d peaks found'%(event_no,peak_no))
+	total_event_no=len(frame_list)
+
+	print('--------------')
+	print(Eiger_file_name)
+	print('the data block shape is: ',img_block.shape)  #h5py read
+	#print('the data block shape is: (%d,%d,%d)'%(len(frame_list),db_1.shape[1],db_1.shape[2])) #fabio read
+	if mask_file!='None':
+		mask_file=os.path.abspath(mask_file)
+		m=h5py.File(mask_file,'r')
+		mask=m['/data/data'][x_min:x_max,y_min:y_max].astype(bool)
+		m.close()
+	elif mask_file=='None':
+		mask=np.ones_like(img_block[0,:,:]).astype(bool)  #h5py read
+		#mask=np.ones_like(db.data).astype(bool)            #fabio read
+	else:
+		sys.exit('the mask file option is inproper.')
+	HIT_counter=0
+	HIT_event_no_list=[]
+	peakXPosRaw=np.zeros((len(frame_list),1024))
+	peakYPosRaw=np.zeros((len(frame_list),1024))
+	pixel_size=np.float(110e-6)#to be changed
+	nPeaks=np.zeros((len(frame_list),),dtype=np.int16)
+	peakTotalIntensity=np.zeros((len(frame_list),1024))
+
+	for l in range(len(frame_list)):
+		event_no=frame_list[l]
+		img_arry=img_block[l,:,:]  #h5py read
+		#db.get_frame(event_no)     #fabio read
+		#img_arry=np.array(db.data) #fabio read
+		bimg=(img_arry>thld)
+		bimg=bimg*mask
+		all_labels=measure.label(bimg)
+		props=measure.regionprops(all_labels,img_arry)
+		area=np.array([r.area for r in props]).reshape(-1,)
+		max_intensity=np.array([r.max_intensity for r in props]).reshape(-1,)
+		label=np.array([r.label for r in props]).reshape(-1,)
+		centroid=np.array([np.array(r.centroid).reshape(1,2) for r in props]).reshape((-1,2))
+		weighted_centroid=np.array([r.weighted_centroid for r in props]).reshape(-1,)
+		label_filtered=label[(area>=min_pix)*(area<max_pix)]
+		area_filtered=area[(area>=min_pix)*(area<max_pix)]
+		area_sort_ind=np.argsort(area_filtered)[::-1]
+		label_filtered_sorted=label_filtered[area_sort_ind]
+		area_filtered_sorted=area_filtered[area_sort_ind]
+		weighted_centroid_filtered=np.zeros((len(label_filtered_sorted),2))
+		max_intensity_filtered=np.zeros((len(label_filtered_sorted),1))
+		mean_intensity_filtered=np.zeros((len(label_filtered_sorted),1))
+
+		peak_no=len(area_filtered_sorted)
+		nPeaks[l]=np.int16(np.minimum(1024,peak_no))
+		for index,value in enumerate(label_filtered_sorted):
+			weighted_centroid_filtered[index,:]=np.array(props[value-1].weighted_centroid)
+			max_intensity_filtered[index,:]=props[value-1].max_intensity
+			mean_intensity_filtered[index,:]=props[value-1].mean_intensity
+
+			peakTotalIntensity[l,:nPeaks[l]]=mean_intensity_filtered[:nPeaks[l],0].reshape(-1,)
+			peakXPosRaw[l,:nPeaks[l]]=weighted_centroid_filtered[:nPeaks[l],0]+x_min
+			peakYPosRaw[l,:nPeaks[l]]=weighted_centroid_filtered[:nPeaks[l],1]+y_min
+        #peak_no=len(area_filtered_sorted)
+		if (peak_no>=min_peak) and (peak_no<=1024):
+			HIT_counter+=1
+			HIT_event_no_list.append(event_no)
+			print('HIT!!!!  %d  Event: %d peaks found'%(event_no,peak_no))
+		else:
+			print('BLANK!   %d  Event: %d peaks found'%(event_no,peak_no))
             #pass
-    print(Eiger_file_name)
-    print('%d   out of  %d  hits found!'%(HIT_counter,img_block.shape[0]))
-    print('HIT rate: %.2f %%'%(100*HIT_counter/img_block.shape[0]))
-    print('HIT events:')
-    print(HIT_event_no_list)
-	
+	peak_list_dict={'Eiger_file_name':Eiger_file_name,'frame_list':frame_list,\
+	'HIT_event_no_list':HIT_event_no_list,'nPeaks':nPeaks.astype(np.int16),\
+	'peakTotalIntensity':peakTotalIntensity,\
+	'peakXPosRaw':peakXPosRaw,'peakYPosRaw':peakYPosRaw}
+	print(Eiger_file_name)
+	print('%d   out of  %d  hits found!'%(HIT_counter,img_block.shape[0]))
+	print('HIT rate: %.2f %%'%(100*HIT_counter/img_block.shape[0]))
+	print('HIT events:')
+	print(HIT_event_no_list)
+
 	#lf=open(os.path.split(Eiger_file_name)[1]+'HIT.log','w')
 	#lf.write('%s'%(Eiger_file_name))
 	#lf.write('\n %d   out of  %d  hits found!'%(HIT_counter,img_block.shape[0]))
@@ -172,71 +203,85 @@ def file_hit_finder_subset(Eiger_file_name,rank,size,skip,thld,min_pix,min_peak,
 		#lf.write('%d \n'%event)
 	#lf.write('-----------------')
 	#lf.close()
-    total_event_no=img_block.shape[0]
-    hit_rate=100*HIT_counter/total_event_no
-    return total_event_no, HIT_counter, hit_rate, HIT_event_no_list
+	#total_event_no=img_block.shape[0]
+	hit_rate=100*HIT_counter/total_event_no
+	return total_event_no, HIT_counter, hit_rate, HIT_event_no_list, peak_list_dict
 
 def Eiger_file_list(find_list_file):
 	find_list_file=os.path.abspath(find_list_file)
 	l=open(find_list_file,'r')
 	list_s=l.readlines()
-	
+
 	return list_s
 
 #if __name__=='__main__':
-	
+
 #	Eiger_file_name=sys.argv[1]
 #	frame_no=int(sys.argv[2])
 #	thld=int(sys.argv[3])
 #	min_pix=int(sys.argv[4])
 #	mask_file=sys.argv[5]
 #	img_show_flag=(sys.argv[6]=='True')
-	
+
 #	img_arry=Eiger_img_read(Eiger_file_name,frame_no)
 #	label_filtered_sorted,weighted_centroid_filtered,props=peak_finder(img_arry,Eiger_file_name,frame_no,thld,min_pix,mask_file=mask_file,interact=img_show_flag)
-	
+
 #	print('In image: %s \n %5d peaks are found' %(Eiger_file_name, len(label_filtered_sorted)))
 
 
 
 if __name__=='__main__':
-    t = TicToc()
-    t.tic()
+	t = TicToc()
+	t.tic()
 
-    comm=MPI.COMM_WORLD
-    size=comm.Get_size()
-    rank=comm.Get_rank()
-    print('I am rank %d of %d'%(rank,size))
-    find_list_file=sys.argv[1]
-    find_list_file=os.path.abspath(find_list_file)
-    list_s=Eiger_file_list(find_list_file)
+	comm=MPI.COMM_WORLD
+	size=comm.Get_size()
+	rank=comm.Get_rank()
+	print('I am rank %d of %d'%(rank,size))
+	find_list_file=sys.argv[1]
+	find_list_file=os.path.abspath(find_list_file)
+	list_s=Eiger_file_list(find_list_file)
     #Eiger_file_name=sys.argv[1]
-    thld=int(sys.argv[2])
-    min_pix=int(sys.argv[3])
-    mask_file=sys.argv[4]
-    min_peak=int(sys.argv[5])
-    Region=sys.argv[6]
-    skip=1
-    lf=open(os.path.split(find_list_file)[1]+'HIT-rank%d.log'%(rank),'w',1)
-    lf.write('Eiger file list: %s\n'%(find_list_file))
-    lf.write('thld: %d\n'%(thld))
-    lf.write('min_pix: %d\n'%(min_pix))
-    lf.write('min_peak: %d\n'%(min_peak))
-    lf.write('mask_file: %s\n'%(mask_file))
-    ef=open(os.path.split(find_list_file)[1]+'eve-rank%d.lst'%(rank),'w',1)
-    for l in range(len(list_s)):
-        Eiger_file_name=list_s[l][:-1]
-        
-        print('hit finding %d file  out of %d    \n%s'%(l+1,len(list_s),list_s[l]))
-        
-        total_event_no, HIT_counter, hit_rate,HIT_event_no_list=file_hit_finder_subset(list_s[l][:-1],rank,size,skip,thld,min_pix,min_peak,mask_file=mask_file,Region=Region)
-        lf.write('%s'%(Eiger_file_name))
-        lf.write('\n %d   out of  %d  hits found!'%(HIT_counter,total_event_no))
-        lf.write('\n HIT rate: %.2f %%\n'%(hit_rate))
-        for event in HIT_event_no_list:
-            ef.write('%s //%d\n'%(Eiger_file_name,event))
-    lf.close()
-    ef.close()
-    
-    t.toc('Worker %d took'%(rank))
-    print('!!!!ALL DONE!!!')
+	thld=int(sys.argv[2])
+	min_pix=int(sys.argv[3])
+	max_pix=int(sys.argv[4])
+	mask_file=sys.argv[5]
+	min_peak=int(sys.argv[6])
+	Region=sys.argv[7]
+	skip=1
+	lf=open(os.path.split(find_list_file)[1]+'HIT-rank%d.log'%(rank),'w',1)
+	lf.write('Eiger file list: %s\n'%(find_list_file))
+	lf.write('thld: %d\n'%(thld))
+	lf.write('min_pix: %d\n'%(min_pix))
+	lf.write('max_pix: %d\n'%(max_pix))
+	lf.write('min_peak: %d\n'%(min_peak))
+	lf.write('mask_file: %s\n'%(mask_file))
+	ef=open(os.path.split(find_list_file)[1]+'eve-rank%d.lst'%(rank),'w',1)
+	pf=open(os.path.split(find_list_file)[1]+'-rank%d.pk'%(rank),'w',1)
+	pf_header='Eiger_file_name event_no hit_tag peak_no peak_id peak_x peak_y peak_Int\n'
+
+	pf.write(pf_header)
+
+	for l in range(len(list_s)):
+		Eiger_file_name=list_s[l][:-1]
+		print('hit finding %d file  out of %d    \n%s'%(l+1,len(list_s),list_s[l]))
+		total_event_no, HIT_counter, hit_rate,HIT_event_no_list,peak_list_dict=\
+		file_hit_finder_subset(list_s[l][:-1],rank,size,skip,thld,min_pix,max_pix,min_peak,mask_file=mask_file,Region=Region)
+		lf.write('%s'%(Eiger_file_name))
+		lf.write('\n %d   out of  %d  hits found!'%(HIT_counter,total_event_no))
+		lf.write('\n HIT rate: %.2f %%\n'%(hit_rate))
+		for event in HIT_event_no_list:
+			ef.write('%s //%d\n'%(Eiger_file_name,event))
+		for l in range(len(peak_list_dict['frame_list'])):
+			event=peak_list_dict['frame_list'][l]
+			hit_tag=int(event in HIT_event_no_list)
+			for peak_id in range(peak_list_dict['nPeaks'][l]):
+				pf.write('%s %d %d %d %d %.2f %.2f %.2f\n'\
+				%(peak_list_dict['Eiger_file_name'],event,hit_tag,peak_list_dict['nPeaks'][l],peak_id,\
+				peak_list_dict['peakXPosRaw'][l][peak_id],peak_list_dict['peakYPosRaw'][l][peak_id],peak_list_dict['peakTotalIntensity'][l][peak_id]))
+	lf.close()
+	ef.close()
+	pf.close()
+
+	t.toc('Worker %d took'%(rank))
+	print('!!!!ALL DONE!!!')
--- comput_info.py
+++ comput_info.py
@@ -47,13 +47,16 @@ class Folder(object):
             txts=txts+l
 
         lines=re.findall('Worker .* took .*',txts)
+        re.findall('Eiger file list: .*',txts)
 
         comp_time_lst=[[],[]]
         for entry in lines:
             t=re.findall('[-+]?\d+\.?\d{0,8}',entry)
             comp_time_lst[0].append(int(t[0]))
             comp_time_lst[1].append(float(t[1]))
-        size=len(comp_time_lst[0])
+
+        log_lst=glob.glob(self.folder_name+'/*rank*.log')
+        size=len(log_lst)
         return {'num_workers':size,'computation_time_list':comp_time_lst}
     @property
     def comp_time_mean(self):
@@ -70,6 +73,95 @@ class Folder(object):
         num_cores=self.comp_time_lst['num_workers']
         comp_time_max=np.array(self.comp_time_lst['computation_time_list'][1]).max()
         return comp_time_max
+
+    def Get_hit_stats_single(self,rank):
+        log_file=glob.glob(self.folder_name+'/*-rank%d.log'%(rank))[0]
+        fl=open(log_file,'r')
+        txt=fl.readlines()
+        fl.close()
+        Eiger_file_list_file=txt[0][:-1].split(': ',1)[1]
+        Eiger_file_run_lst=[]
+        Hit_count=[]
+        Eve_count=[]
+        Hit_rate=[]
+        for l in range(len(txt)):
+            ll=txt[l]
+            t=re.findall('data_.*\.h5',ll)
+
+            if len(t)!=0:
+                Eiger_file=ll[:-1]
+                Eiger_file_short=os.path.basename(Eiger_file)
+                Eiger_file_pre=Eiger_file_short.split('_data_',1)[0]
+                Eiger_file_run=int(Eiger_file_short.split('_data_',1)[1].split('.',1)[0])
+                Eiger_file_run_lst.append(Eiger_file_run)
+                if (l+1)<len(txt):
+                    Hit_count.append(re.findall('\d+',txt[l+1])[0])
+                    Eve_count.append(re.findall('\d+',txt[l+1])[1])
+                else:
+                    Hit_count.append(0)
+                    Eve_count.append(0)
+                if (l+2)<len(txt):
+                    Hit_rate.append(re.findall('\d+\.\d+',txt[l+2])[0])
+                else:
+                    Hit_rate.append(0)
+
+        ind=np.argsort(Eiger_file_run_lst)
+        Eiger_file_run_lst=np.array(Eiger_file_run_lst)[ind]
+        Hit_count=np.array(Hit_count)[ind]
+        Eve_count=np.array(Eve_count)[ind]
+        Hit_rate=np.array(Hit_rate)[ind]
+        return {'Eiger_file_list_file':Eiger_file_list_file,'Eiger_file_pre':Eiger_file_pre,'Eiger_file_run_lst':Eiger_file_run_lst,\
+                'Hit_count':Hit_count,'Eve_count':Eve_count,'Hit_rate':Hit_rate}
+    def Get_peak_raw_stats_single(self,rank):
+        pk_file=glob.glob(self.folder_name+'/*-rank%d.pk'%(rank))[0]
+        peak_arry=np.genfromtxt(pk_file,skip_header=1)
+        peak_arry=peak_arry[:,1:]
+        return peak_arry
+
+    def Get_peak_raw_stats_all(self):
+        size=self.comp_time_lst['num_workers']
+        Eiger_file_pre=self.Get_hit_stats_single(0)['Eiger_file_pre']
+        peak_arry_all=np.array([]).reshape(-1,7)
+        for rank in range(size):
+            peak_arry=self.Get_peak_raw_stats_single(rank)
+            peak_arry_all=np.concatenate((peak_arry_all,peak_arry),axis=0)
+        return Eiger_file_pre,peak_arry_all
+
+    def Get_peak_stats_all(self,cam_len=0.2,photon_energy=12000,pixel_size=100e-6,center_x=2185,center_y=2075):
+        Eiger_file_pre,peak_arry_all=self.Get_peak_raw_stats_all()
+        RD=np.sqrt((peak_arry_all[:,-3]-center_x)**2+(peak_arry_all[:,-2]-center_y)**2)
+        scat_ang=np.rad2deg(np.arctan(RD*pixel_size/cam_len))
+        wave_len=12398/photon_energy
+        Res=(wave_len)/(2*np.sin(np.deg2rad(scat_ang)/2)) #in Angstrom
+        peak_arry_new=np.concatenate((peak_arry_all,RD.reshape(-1,1),scat_ang.reshape(-1,1),Res.reshape(-1,1)),axis=-1)
+        return Eiger_file_pre, peak_arry_new
+
+    def Get_hit_stats_all(self):
+        '''
+        live mode: when the hit finding is still running.
+        '''
+        Eiger_file_list_file=(self.Get_hit_stats_single(0))['Eiger_file_list_file']
+        f=open(Eiger_file_list_file,'r')
+        Eiger_file_lst=f.readlines()
+        f.close()
+        num_Eiger_file=len(Eiger_file_lst)
+
+        size=self.comp_time_lst['num_workers']
+        Hit_count_all=np.ones((num_Eiger_file,size))*(-10)
+        Hit_rate_all=np.ones((num_Eiger_file,size))*(-10)
+
+        for rank in range(size):
+            single_dict=self.Get_hit_stats_single(rank)
+            Hit_count_ary=np.array(single_dict['Hit_count']).reshape(-1,)
+            Hit_count_all[0:Hit_count_ary.shape[0],rank]=Hit_count_ary
+            Hit_rate_ary=np.array(single_dict['Hit_rate']).reshape(-1,)
+            Hit_rate_all[0:Hit_rate_ary.shape[0],rank]=Hit_rate_ary
+
+        Hit_rate_all_flat=Hit_rate_all.reshape(-1,)
+        Hit_count_all_flat=Hit_count_all.reshape(-1,)
+        return num_Eiger_file,Hit_count_all_flat,Hit_rate_all_flat
+
+
 
 
 
@@ -89,7 +181,20 @@ if __name__=='__main__':
         max_time.append(vars()[short_name+'_obj'].comp_time_max)
         min_time.append(vars()[short_name+'_obj'].comp_time_min)
 
-    plt.figure(1)
+
+    Eiger_file_run_lst=np.array(vars()[short_name+'_obj'].Get_hit_stats_single(0)['Eiger_file_run_lst'])
+    Eiger_file_pre=vars()[short_name+'_obj'].Get_hit_stats_single(0)['Eiger_file_pre']
+
+    num_Eiger_file,Hit_count_all_flat,Hit_rate_all_flat=vars()[short_name+'_obj'].Get_hit_stats_all()
+
+    #print(vars()[short_name+'_obj'].Get_hit_stats_single(0)['Hit_count'])
+    #print(vars()[short_name+'_obj'].Get_hit_stats_single(0)['Eiger_file_run_lst'])
+
+
+
+
+    plt.figure(num=1,figsize=(10,20))
+    plt.subplot(2,1,1)
     plt.title('NSLS_hit_finding computation time plot')
     plt.plot(num_cores,mean_time,'r+',num_cores,max_time,'gs',num_cores,min_time,'b^')
     plt.xlabel('number of cores used for computation')
@@ -97,5 +202,19 @@ if __name__=='__main__':
     leg_label=['mean_time','max_time','min_time']
     plt.legend(leg_label,loc='upper right')
     plt.xticks(np.arange(0,110,step=10))
-    plt.show()
+    plt.subplot(2,1,2)
+    plt.plot(num_cores,np.array(mean_time)/(num_Eiger_file*500),'r+',num_cores,np.array(max_time)/(num_Eiger_file*500),'gs',num_cores,np.array(min_time)/(num_Eiger_file*500),'b^')
+    plt.xlabel('number of cores used for computation')
+    plt.ylabel('computation time per pattern')
+    leg_label=['mean_time','max_time','min_time']
+    plt.legend(leg_label,loc='upper right')
+    plt.xticks(np.arange(0,110,step=10))
     plt.savefig('comp_time.png')
+
+    plt.figure(num=2)
+    plt.plot(Hit_rate_all_flat,'bs',markersize=1)
+    plt.xlabel('chucks of Eiger events in the *.lst file, in time series')
+    plt.ylabel('Hit_rate %')
+    plt.title(Eiger_file_pre)
+    plt.savefig('Hit_rate.png')
+    plt.show()
